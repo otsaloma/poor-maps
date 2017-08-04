1e825774b62e7a51e77c794143ffd6661a19549e
Rinigus <rinigus.git@gmail.com>
2017-08-04 19:59:01 +0300

    specify that transit mode display does not support reroute

--- poor/narrative.py
+++ poor/narrative.py
@@ -360,7 +360,8 @@ class Narrative:
                     man_time=man_time,
                     icon=icon,
                     narrative=narrative,
-                    direction=direction)
+                    direction=direction,
+                    reroute=False)
 
     def get_maneuvers(self, x, y):
         """Return a list of dictionaries of maneuver details."""
